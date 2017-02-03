41724a775e06d7984109b7c010d1e1fa09ee00dc
Chenshu <ycswaves@gmail.com>
2017-02-03 21:32:13 +0800

    fix android err caused by rnsound

--- android/app/src/main/java/com/pushup/MainApplication.java
+++ android/app/src/main/java/com/pushup/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.zmxv.RNSound.RNSoundPackage;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+              new RNSoundPackage()
       );
     }
   };
